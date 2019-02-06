0538c37f3bf47e3c9b3870ef767d0110d2acc6b2
Jahangir Kabir <jahangirkobir@gmail.com>
2019-02-06 11:39:41 +0600

    added text edit functionality

--- iXplorer/DocumentViewController.swift
+++ iXplorer/DocumentViewController.swift
@@ -13,10 +13,19 @@ class DocumentViewController: UIViewController {
     @IBOutlet weak var detailTextView: UITextView!
     @IBOutlet weak var documentImage: UIImageView!
     @IBOutlet weak var documentNameLabel: UILabel!
-    
+    @IBOutlet weak var documentSaveButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
     var document: Document?
     
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        detailTextView.backgroundColor = UIColor.white
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
         
         document?.open(completionHandler: { (success) in
@@ -27,12 +36,14 @@ class DocumentViewController: UIViewController {
                 if self.document?.fileType == "public.png" || self.document?.fileType == "public.jpg" || self.document?.fileType == "public.jpeg" {
                     
                     self.detailTextView.isHidden = true
+                    self.saveButton.isHidden = true
                     self.documentImage.isHidden = false
                     
                     self.documentImage.image = UIImage(data: (self.document?.fileData)!)
                 } else if self.document?.fileType == "public.plain-text" {
                     
                     self.detailTextView.isHidden = false
+                    self.saveButton.isHidden = false
                     self.documentImage.isHidden = true
                     
                     self.detailTextView.text = self.document?.filesText!
@@ -43,9 +54,26 @@ class DocumentViewController: UIViewController {
         })
     }
     
+    @IBAction func save(_ sender: Any) {
+        self.document?.filesText = self.detailTextView.text
+        print("Save to: \(String(describing: self.document?.fileURL.path))!")
+        
+        self.document?.save(to: (self.document?.fileURL)!, for: .forOverwriting,
+                            completionHandler: {(success: Bool) -> Void in
+                                if success {
+                                    print("File created OK")
+                                    self.dismissDocumentViewController()
+                                } else {
+                                    print("Failed to save file.")
+                                    self.detailTextView.backgroundColor = UIColor.red
+                                }
+        })
+    }
+    
     @IBAction func dismissDocumentViewController() {
         dismiss(animated: true) {
             self.document?.close(completionHandler: nil)
         }
     }
+    
 }
